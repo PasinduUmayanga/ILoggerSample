2715a68d1f339a827f7f96174183f1ce4673632b
PasinduUmayanga <pasinduumayanga93@gmail.com>
2022-10-14 00:23:14 +0530

    add initialize class libraries

---
+++ src/ILS.Application/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ILS.Application
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddLoggerApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
